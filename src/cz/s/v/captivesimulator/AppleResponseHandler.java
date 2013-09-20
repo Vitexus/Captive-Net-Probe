eb9d1f77d9ed3313c8763f335864e466c76a0bc9
Vitex <vitex@hippy.cz>
2013-09-20 18:17:07 +0200

    Základní funkcionalita

---
+++ src/cz/s/v/captivesimulator/AppleResponseHandler.java
@@ -0,0 +1,195 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.s.v.captivesimulator;
+
+import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author vitex
+ */
+public class AppleResponseHandler extends AsyncHttpResponseHandler {
+
+    /**
+     * Adresa testu
+     */
+    public String testUrl = "http://www.apple.com/library/test/success.html";
+
+    /**
+     * Správná SSL odpověď
+     */
+    String responseSuccessAppleSSL = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2//EN\">\n"
+            + "<HTML>\n"
+            + "<HEAD>\n"
+            + "	<TITLE>Success</TITLE>\n"
+            + "</HEAD>\n"
+            + "<BODY>\n"
+            + "Success\n"
+            + "</BODY>\n"
+            + "</HTML>\n";
+
+    /**
+     * Správná SSL odpověď
+     */
+    String responseSuccessApple = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>";
+
+    public String responseBody = "";
+
+    private TestCaptiveNetwork appContext;
+
+    public AppleResponseHandler(TestCaptiveNetwork context) {
+        appContext = context;
+    }
+
+    @Override
+    public void onStart() {
+        appContext.testTryNumber++;
+        appContext.testTryResult = -1;
+        TextView text = (TextView) appContext.findViewById(R.id.result);
+        text.setText("Test under progress. Please wait ...");
+
+        MenuItem statusIcon = appContext.menu.findItem(R.id.action_icon);
+        statusIcon.setIcon(android.R.drawable.ic_menu_rotate);
+
+        WebView myWebView = (WebView) appContext.findViewById(R.id.webview);
+        myWebView.loadData("?", "text/plain", "utf-8");
+        Toast.makeText(appContext.getBaseContext(), "#" + appContext.testTryNumber + " test", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSuccess(String response) {
+
+        responseBody = response;
+
+        if (response.equals(responseSuccessApple)) {
+            appContext.testTryResult = 2;
+
+        } else {
+            response.length();
+            responseSuccessApple.length();
+
+            appContext.testTryResult = 1;
+        }
+
+        TextView responseCode = (TextView) appContext.findViewById(R.id.response);
+        responseCode.setMovementMethod(new ScrollingMovementMethod());
+        responseCode.setText(response.toString());
+
+    }
+
+    @Override
+    public void onFailure(Throwable e, String response) {
+        Toast.makeText(appContext.getBaseContext(), response.toString() + " Error: " + e.toString(), Toast.LENGTH_SHORT).show();
+        appContext.testTryResult = 0;
+        TextView text = (TextView) appContext.findViewById(R.id.response);
+        text.setText(response.toString());
+
+    }
+
+    @Override
+    public void onFinish() {
+        Toast.makeText(appContext.getBaseContext(), "Finish", Toast.LENGTH_SHORT).show();
+        TextView resumeText = (TextView) appContext.findViewById(R.id.result);
+
+        WebView myWebView = (WebView) appContext.findViewById(R.id.webview);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setAppCacheEnabled(false);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.clearHistory();
+        myWebView.clearFormData();
+        myWebView.clearCache(true);
+        myWebView.setWebViewClient(new RedirectingWebViewClient(appContext));
+
+        Map<String, String> noCacheHeaders = new HashMap<String, String>(2);
+        noCacheHeaders.put("Pragma", "no-cache");
+        noCacheHeaders.put("Cache-Control", "no-cache, must-revalidate, no-store");
+
+        MenuItem statusIcon = appContext.menu.findItem(R.id.action_icon);
+
+        if (appContext.testTryResult == 2) {
+            statusIcon.setIcon(R.drawable.ic_state_online);
+            resumeText.setText("Online :)");
+            myWebView.loadUrl(testUrl, noCacheHeaders);
+        }
+
+        if (appContext.testTryResult == 1) {
+            statusIcon.setIcon(R.drawable.ic_state_blocked);
+            resumeText.setText("Blocked :|");
+            myWebView.loadUrl(testUrl, noCacheHeaders);
+
+        }
+
+        if (appContext.testTryResult == 0) {
+            statusIcon.setIcon(R.drawable.ic_state_unknown);
+            resumeText.setText("Offline :(");
+            myWebView.loadData("Network error...", "text/html", "utf-8");
+        }
+
+    }
+
+    /**
+     * WebViev that can handle redirects
+     */
+    private class RedirectingWebViewClient extends WebViewClient {
+
+        private final TestCaptiveNetwork appContext;
+
+        private RedirectingWebViewClient(TestCaptiveNetwork context) {
+            appContext = context;
+        }
+
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            if (url.equals(appContext.testUrl)) {
+                appContext.testTryNumber++;
+            }
+            TextView text = (TextView) appContext.findViewById(R.id.result);
+            text.setText("WebView loading url: " + url);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            if (url.equals(appContext.testUrl)) {
+                Toast.makeText(appContext, "Page Loaded ... #" + appContext.testTryNumber, Toast.LENGTH_SHORT).show();
+
+                /*            
+                 long interval = System.currentTimeMillis() - m_start;
+                 Toast.makeText(appContext, "Loaded this webpage [" + refreshCount + "] "
+                 + "times in [" + interval + "] ms", Toast.LENGTH_SHORT).show();
+                 */
+                if (appContext.testTryResult == 1) { //Was BLOCKED - try again
+                    if (appContext.testTryNumber == 2) {
+                        AsyncHttpClient secondTryClient = new AsyncHttpClient();
+                        secondTryClient.setUserAgent("CaptiveNetworkSupport-209.39 wispr" + appContext.testTryNumber);
+                        secondTryClient.get(testUrl, new AppleResponseHandler(appContext));
+                    }
+                }
+
+            }
+        }
+    }
+
+}
--- src/cz/s/v/captivesimulator/TestCaptiveNetwork.java
+++ src/cz/s/v/captivesimulator/TestCaptiveNetwork.java
@@ -8,16 +8,21 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class TestCaptiveNetwork extends Activity {
+
+    public Menu menu;
+    /**
+     * Pořadové číslo testu
+     */
+    public int testTryNumber = 0;
+
+    /**
+     * Výsledek: -1 netestovano, 0 chyba, 1 blokovano, 2 ok
+     */
+    public int testTryResult = -1;
 
     public String testUrl = "http://www.apple.com/library/test/success.html";
 //            testUrl = "http://v.s.cz/success.html";
@@ -36,13 +41,15 @@ public class TestCaptiveNetwork extends Activity {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
+        this.menu = menu;
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
+
         switch (item.getItemId()) {
+
             case R.id.action_refresh:
                 if (isNetworkAvailable()) {
                 performAppleTest();
@@ -53,6 +60,7 @@ public class TestCaptiveNetwork extends Activity {
                 return true;
 
             case R.id.action_settings:
+                Toast.makeText(getBaseContext(), "Not yet implemented ", Toast.LENGTH_SHORT).show();
 //                openSettings();
                 return true;
             default:
@@ -83,128 +91,12 @@ public class TestCaptiveNetwork extends Activity {
     }
 
     /**
-     * Výsledek: -1 netestovano, 0 chyba, 1 blokovano, 2 ok
-     */
-    public int testResult = -1;
-
-    /**
      * Otestuje Captive capability na síti
-     *
-     * @return test result
      */
     public void performAppleHit() {
-
-        Toast.makeText(getBaseContext(), "First attempt " + testUrl, Toast.LENGTH_SHORT).show();
-
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.setUserAgent("CaptiveNetworkSupport-209.39 wispr");
-        client.get(testUrl, new AsyncHttpResponseHandler() {
-            String responseSuccessAppleSSL = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2//EN\">\n"
-                    + "<HTML>\n"
-                    + "<HEAD>\n"
-                    + "	<TITLE>Success</TITLE>\n"
-                    + "</HEAD>\n"
-                    + "<BODY>\n"
-                    + "Success\n"
-                    + "</BODY>\n"
-                    + "</HTML>\n";
-
-            String responseSuccessApple = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>";
-
-            public String responseBody = "";
-
-            @Override
-            public void onStart() {
-                testResult = -1;
-                TextView text = (TextView) findViewById(R.id.result);
-                text.setText("Test under progress. Please wait ...");
-                WebView myWebView = (WebView) findViewById(R.id.webview);
-
-                myWebView.clearHistory();
-                myWebView.clearFormData();
-                myWebView.clearCache(true);
-
-                WebSettings webSettings = myWebView.getSettings();
-                webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-
-                myWebView.loadData("?", "text/plain", "utf-8");
-            }
-
-            @Override
-            public void onSuccess(String response) {
-
-                responseBody = response;
-
-                if (response.equals(responseSuccessApple)) {
-                    testResult = 2;
-
-                } else {
-                    response.length();
-                    responseSuccessApple.length();
-
-                    testResult = 1;
-                }
-
-                TextView responseCode = (TextView) findViewById(R.id.response);
-                responseCode.setMovementMethod(new ScrollingMovementMethod());
-                responseCode.setText(response.toString());
-
-            }
-
-            @Override
-            public void onFailure(Throwable e, String response) {
-                Toast.makeText(getBaseContext(), response.toString() + " Error: " + e.toString(), Toast.LENGTH_SHORT).show();
-                testResult = 0;
-                TextView text = (TextView) findViewById(R.id.response);
-                text.setText(response.toString());
-            }
-
-            @Override
-            public void onFinish() {
-                Toast.makeText(getBaseContext(), "Finish", Toast.LENGTH_SHORT).show();
-                TextView resumeText = (TextView) findViewById(R.id.result);
-
-                    WebView myWebView = (WebView) findViewById(R.id.webview);
-                    WebSettings webSettings = myWebView.getSettings();
-                    webSettings.setJavaScriptEnabled(true);
-                    webSettings.setBuiltInZoomControls(true);
-                    myWebView.clearHistory();
-                    myWebView.clearFormData();
-                    myWebView.clearCache(true);
-                    webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-                
-                
-                if (testResult == 2) {
-                    resumeText.setText("Online :)");
-                    myWebView.loadUrl(testUrl);
-                }
-
-                if (testResult == 1) {
-                    resumeText.setText("Blocked :|");
-                    myWebView.loadUrl(testUrl);
-                }
-
-                if (testResult == 0) {
-                    resumeText.setText("Offline :(");
-                    myWebView.loadData("Network error...", "text/html", "utf-8");
-                }
-
-            }
-
-        });
-
-    }
-
-    /**
-     * WebViev that can handle redirects
-     */
-    private class RedirectingWebViewClient extends WebViewClient {
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+        AsyncHttpClient firstTryClient = new AsyncHttpClient();
+        firstTryClient.setUserAgent("CaptiveNetworkSupport-209.39 wispr");
+        firstTryClient.get(testUrl, new AppleResponseHandler(this));
     }
 
     /**
